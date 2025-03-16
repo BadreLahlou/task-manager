89ac619a3323c4545de1bbac22fb1f976c869a02
Badre Lahlou <lhloulh1@gmail.com>
2025-03-16 19:26:44 +0100

    Update Notification.java

--- TM/src/main/java/com/badrelahlou/taskmanager/model/Notification.java
+++ TM/src/main/java/com/badrelahlou/taskmanager/model/Notification.java
@@ -23,7 +23,7 @@ public class Notification {
     private LocalDateTime createdAt;
     private boolean isRead;
 
-    // Getters and Setters
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public User getUser() { return user; }
